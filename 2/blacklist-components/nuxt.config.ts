b598296864eefe46ccc8c30324b32566216cca43
te <te1@users.noreply.github.com>
2024-04-26 01:18:26 +0200

    add blacklist-components

---
+++ 2/blacklist-components/nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: ["@nuxt/ui"],
+
+  features: {
+    inlineStyles: false,
+  },
+
+  vite: {
+    build: {
+      cssCodeSplit: false,
+
+      rollupOptions: {
+        output: {
+          assetFileNames: `_nuxt/[name].[ext]`,
+        },
+      },
+    },
+  },
+});
